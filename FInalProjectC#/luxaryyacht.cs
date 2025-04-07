e387a1afae484bf22c0f683a23087b9ba5c61b4a
AhmedWagha3597 <ahmedwagha35@hotmail.com>
2025-04-07 12:52:14 -0400

    Update luxaryyacht.cs

--- FInalProjectC#/luxaryyacht.cs
+++ FInalProjectC#/luxaryyacht.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eight;
+using two;
 
 namespace FInalProjectC_
 {
-    internal class luxaryyacht
+    // LuxuryYacht.cs (Extends Boat, includes Helipad)
+
+    class LuxuryYacht : boat
     {
+        protected int helipad;
+
+
+        public int gethelipad()
+        {
+            return helipad;
+        }
+
+        public void sethelipad(int hep)
+        {
+            helipad = hep;
+        }
+
+        public LuxuryYacht(string n, double price, double sp, string vtype,int sc, int hep) : base(n, price, sp, vtype, sc)
+        {
+            helipad = hep;
+        }
+        public override double calculateTax()
+        {
+            return price = price * 1;
+        }
     }
 }
