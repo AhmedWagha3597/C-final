cad9272a2af003955d87744469f2f98e2960e365
AhmedWagha3597 <ahmedwagha35@hotmail.com>
2025-04-07 12:51:49 -0400

    Update boat.cs

--- FInalProjectC#/boat.cs
+++ FInalProjectC#/boat.cs
@@ -1,3 +1,4 @@
+using one;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,32 @@ namespace eight
 
 
 
-class boat : vehicles {
-    protected int SeatingCapacity;
+    class boat : vehicles
+    {
+        protected int SeatingCapacity;
 
 
 
 
-public void setseat(int sc) {
-    SeatingCapacity = sc;
+        public void setseat(int sc)
+        {
+            SeatingCapacity = sc;
 
-}
+        }
 
-public int getseat() {
-    return SeatingCapacity;
-}
-public boat(string n, double price, double sp, string vtype,int sc): base(n,price,sp,vtype) {
-    SeatingCapacity= sc;
+        public int getseat()
+        {
+            return SeatingCapacity;
+        }
+        public boat(string n, double price, double sp, string vtype, int sc) : base(n, price, sp, vtype)
+        {
+            SeatingCapacity = sc;
 
-}
+        }
+        public override double calculateTax()
+        {
+            return price = price * 1;
+        }
 
-}
+    }
 }
